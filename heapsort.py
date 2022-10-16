32aacbec373e40a4081029222803dc0ea3526011
sumanta02 <sumanta87@protonmail.com>
2022-10-16 09:52:56 +0530

    Added comments and cleared up the code

--- heapsort.py
+++ heapsort.py
@@ -1,41 +1,41 @@
 def heapsort(list):
-    build_max_heap(list)
+    build_max_heap(list)  # Calling the max heap function to heapify the list
     for i in range(len(list) - 1, 0, -1):
-        list[0], list[i] = list[i], list[0]
-        max_heapify(list, index=0, size=i)
+        list[0], list[i] = list[i], list[0] #swwapping the first and last element of the list
+        max_heapify(list, index=0, size=i)  #and again calling heapify on the root of the list
  
 def parent(i):
-    return (i - 1)//2
+    return (i - 1)//2   # the parent of a element(i) in a list is always located at index (i - 1) // 2 
  
 def left(i):
-    return 2*i + 1
+    return 2*i + 1  #The left child of an element is always located at 2 * i  + 1
  
 def right(i):
-    return 2*i + 2
+    return 2*i + 2  #The right child of an element is always located at 2 * i  + 2
  
 def build_max_heap(list):
     length = len(list)
-    start = parent(length - 1)
+    start = parent(length - 1) 
     while start >= 0:
-        max_heapify(list, index=start, size=length)
+        max_heapify(list, index=start, size=length) #max_heapify is called on each parent node starting from the last parent node and working towards the root.
         start = start - 1
  
 def max_heapify(list, index, size):
-    l = left(index)
-    r = right(index)
+    l = left(index) #left child of index
+    r = right(index)    #Right child of index
     if (l < size and list[l] > list[index]):
-        largest = l
+        largest = l #define left child as largest if it is greater than it's parent
     else:
         largest = index
     if (r < size and list[r] > list[largest]):
-        largest = r
+        largest = r #define right child as largest if it is greater than it's parent
     if (largest != index):
-        list[largest], list[index] = list[index], list[largest]
-        max_heapify(list, largest, size)
+        list[largest], list[index] = list[index], list[largest] # swap the index element with it's right or left child if any of them is larger than the index
+        max_heapify(list, largest, size)    #call max heapify on the new largest index to modify the lower branches to maintain the heap property
  
  
-list = input('Enter the list of numbers: ').split()
-list = [int(x) for x in list]
-heapsort(list)
-print('Sorted list: ', end='')
+list = input('Enter the list of numbers: ').split() #Take user input to get the array of numbers
+list = [int(x) for x in list]   #modify the list to change all elements to integer
+heapsort(list)  #sort the list
+print('Sorted list: ', end='')  #print the sorted list
 print(list)
