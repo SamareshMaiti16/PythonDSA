2fb578a8ba8322b8d1bb103814be2752e23a778e
Sunny Patil <sunnypatil4006@gmail.com>
2022-10-31 14:33:09 +0530

    fractional_knapsack

--- FractionalKnapsack.py
+++ FractionalKnapsack.py
@@ -1,34 +1,32 @@
-class Item:
-    def __init__(self, value, weight):
-        self.value = value
-        self.weight = weight
-def fractionalknapsack(Capacity,arr):
-    arr.sort(key=lambda x: (x.value/x.weight), reverse=True)   
- 
-    # Result(value in Knapsack)
-    finalvalue = 0.0
- 
-    # Looping through all Items
-    for item in arr:
- 
-        # If adding Item won't overflow,
-        # add it completely
-        if item.weight <= Capacity:
-            Capacity -= item.weight
-            finalvalue += item.value
- 
-        # If we can't add current Item,
-        # add fractional part of it
+import numpy as np
+def fractionak_Knapsack(w,v,c):
+    l = list(zip(w,v))
+    # using zip we have clubbed together weights and value as a 2d array
+    x = []
+    for i,j in l:
+        ratio = j/i
+        x.append(ratio)
+        #ratios of value/weight is stored in this list 
+    maximum_val = 0 
+    #to get the max value with given capacity
+    index = list(range(len(weights)))
+    #we are creating list of index of number of elements in weights
+    index.sort(key=lambda i:x[i],reverse=True)
+    #here we map the x(val per unit weight)with the index
+    for i in index:
+        if(c>weights[i]):
+            #if the capacity is greater than the weight at index i then add the value of weight at inex i and reduce the capacity equal to weight added
+            maximum_val+=values[i]
+            c-=weights[i]
         else:
-            finalvalue += item.value * Capacity/ item.weight
+            maximum_val+=c*x[i]
+            # if the capacity is less than the weight at index i then add the product of the remaining capacity and ratio of value /weight at index i
             break
-     
-    # Returning final value
-    return finalvalue
+    print(maximum_val)
+weights=[2,3,4,5]
+values=[10,20,30,40]
+c = 10
+fractionak_Knapsack(weights,values,c)
 
-Capacity = 50
-arr = [Item(90, 10), Item(120, 20), Item(150, 30)]
-    # Function call
-max_val = fractionalknapsack(Capacity, arr)
-print(max_val)
- 
+
+
